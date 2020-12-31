6035e20f7298e4aae083ef92a2ec08f4199e3ad7
Kenneth Ostby <kenneth.ostby@gmail.com>
2020-12-31 15:13:10 -0800

    Finally found some time to attack these again. Day9

---
+++ day9/day9.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+window = []
+
+def check_stuff(n):
+    assert len(window) == 25
+
+    for val in window:
+        i = n-val
+
+        if i in window:
+            return True
+
+    return False
+
+
+with open("input.txt") as fh:
+    for line_num,line in enumerate(fh):
+        line = line.strip()
+        number = int(line)
+
+        print(window)
+        if int(line_num) >= 25:
+            if not check_stuff(number):
+                print(number)
+                break
+            window = window[1:]
+
+        window.append(number)
+
+
