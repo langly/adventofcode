5d7dd2d2da03f9eb7fdafbda1d9df3d730257141
Kenneth Østby <kenneth.ostby@gmail.com>
2024-12-01 16:24:39 -0800

    Added part2

--- 2024/day01_0/src/main.rs
+++ 2024/day01_0/src/main.rs
@@ -1,18 +1,26 @@
 use std::fs::read_to_string;
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
-
     let filename = "input.txt";
 
     let mut va: Vec<i32> = Vec::new();
     let mut vb: Vec<i32> = Vec::new();
+
+    let mut counts: HashMap<i32,i32> = HashMap::new();
 
     for line in read_to_string(filename).unwrap().lines() {
         let s: Vec<&str> = line.split_whitespace().collect();
 
         let a: i32 = s[0].parse().expect("Not a valid number");
         let b: i32 = s[1].parse().expect("Not a valid number");
+
+        if !counts.contains_key(&b) {
+            counts.insert(b,0 as i32);
+        }
+
+        let cnt = counts.get(&b).unwrap() + 1;
+        counts.insert(b, cnt);
 
         va.push(a);
         vb.push(b);
@@ -21,11 +29,27 @@ fn main() {
     va.sort();
     vb.sort();
 
-    let merged: Vec<i32> = va.into_iter().zip(vb.into_iter())
+    let merged: Vec<i32> = va.iter().zip(vb.iter())
                             .map( |(x,y)| (x - y).abs() )
                             .collect();
 
     let total: i32 = merged.into_iter().sum();
+    println!("Part 0 : {total}");
 
-    println!("{total}");
+    // Now part 1
+    let mut part2: i32 = 0;
+
+    for item in va{
+        let v = counts.get(&item);
+
+        if v.is_some() {
+            let part = item * v.unwrap();
+            part2 += part;
+        }
+
+    }
+
+    println!("Part 1: {part2}");
+
+
 }
