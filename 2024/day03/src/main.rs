5a68c4ae8b1862650a0669e5766ff215e6a285a1
Kenneth Østby <kenneth.ostby@gmail.com>
2024-12-02 22:28:59 -0800

    Day03 - part 2

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,27 +1,42 @@
 use regex::Regex;
 use std::fs::read_to_string;
 
+/**
+* Notes: A bit fugly implementation, but could not be
+* bothered to generalize it..
+*/
+
 static mut mul_enable: bool = false;
 
-// fn count_occ2(string: &str) -> i32 {
-//     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
-// 
-//     let mut total : i32 = 0;
-// 
-//     for cap in re.captures_iter(string) {
-// 
-//         println!("{}", cap[0]);
-//         let ci0 : i32 = cap[1].parse().expect("Failed to parse string");
-//         let ci1 : i32 = cap[2].parse().expect("Failed to parse string");
-// 
-//         unsafe {
-//             if mul_enable {
-//                 total +=  ci0 * ci1;
-//             }
-//         }
-//     }
-//     return total;
-// }
+fn count_occ2(string: &str) -> i32 {
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+
+    let mut total : i32 = 0;
+
+    for cap in re.captures_iter(string) {
+
+        unsafe {
+            if &cap[0] == "do()" {
+                mul_enable = true;
+                continue;
+            } else if &cap[0] == "don't()" {
+                mul_enable = false;
+                continue;
+            }
+        }
+
+        // Otherwise we are in a MUL(x,y) section
+        let ci0 : i32 = cap[1].parse().expect("Failed to parse string");
+        let ci1 : i32 = cap[2].parse().expect("Failed to parse string");
+
+        unsafe {
+            if mul_enable {
+                total +=  ci0 * ci1;
+            }
+        }
+    }
+    return total;
+}
 
 fn count_occ(string: &str) -> i32 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
@@ -52,8 +67,11 @@ fn main() {
 
     println!("Part 1: {}", total);
 
-//    for line in read_to_string(filename).unwrap().lines() {
-//        total += count_occ2(&line);
-//    }
+    total = 0;
+    for line in read_to_string(filename).unwrap().lines() {
+        total += count_occ2(&line);
+    }
+
+    println!("Part 2: {}", total);
 
 }
